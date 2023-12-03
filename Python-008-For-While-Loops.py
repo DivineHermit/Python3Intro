a0469ed4994015dc7f92e40a6c03df808cda877c
Dominic Lee <34548479+DivineHermit@users.noreply.github.com>
2023-12-03 19:03:58 +0000

    First draft of 008

--- Python-008-For-While-Loops.py
+++ Python-008-For-While-Loops.py
@@ -4,5 +4,42 @@
     Written by: Dominic Lee
     Date: 
 """
-# ---While Loops
-# ---For Loops
+print("WHILE LOOPS".center(50, "+"))  # Output separator
+# While Loops will repeat a section of code over and over and over and over... until a specified condition is met.
+count = 0  # create a counter variable to keep track of how many loops we've done
+while count <= 10:  # start a while loop with the 'while' keyword and specify the condition to check, 'count' is less then or equal to 10,
+    print(count)  # print out the current value of 'count'
+    count += 1  # increase 'count' by 1 (see section 005-Operators if you need a reminder)
+    # if 'count' is 10 or less the code will go back to line 9 and loop
+print("First loop done!")  # This, and following lines, of code is only ran when 'count' has a value of 11 and the loop finishes. 
+
+# Lets take a look at how to stop a loop early:
+
+while count <= 1_000_000_000:  # loop one(1) BILLION time, ooh thats a lot of loops, I'm dizzy just thinking about it!
+    if count == 101:  # an if statements checks the current value of 'count' and triggers when 'count' is 101
+        break  # the keyword 'break' will stop the loop regardless of the original condition, in this case: count <= 1_000_000_000
+    print(count)  # just print out count till the loop stops, so we can see what is happening.
+    count += 1  # make sure to increase 'count' each time we loop, or we'll get stuck in an infinite loop.
+print("Second loop finished!")  # This and following lines only run when the second loop finishes.
+
+# We can also you the boolean value True to create an infinite loop:
+count = 0  # reset the counter.
+while True:
+    # Since True is always true, this loop will run forever.
+    if count > 10: break  # so we need to include a way to stop the loop, when count gets to eleven(11) this will break the loop.
+    print(count)  # print out the counter so we can see what is happening.
+    count += 1  # increase the count each time through the loop.
+
+print("FOR LOOPS".center(50, "+"))  # Output separator
+print("Range example:")
+# A 'for' loop is used to iterate over an iterable... thats programmer speak for a list or range of values/data.
+for x in range(11):  # each time this loop runs the next value in the range will get saved to 'x'
+    print("x =", x)
+
+print("List:")
+fruits = ["Apple", "Orange", "Grape", "Mellon", "Banana"]
+for fruit in fruits:
+    print("fruit =", fruit)
+
+# For loops are generally faster than While loops but you should built in functions if available as they are much faster.
+# Example, instead of looping over a list of numbers to add them all together, use the built in sum() function.
